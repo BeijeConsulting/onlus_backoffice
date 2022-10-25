3bb01fbc75c25b852fd18cc643daa5552a5159e5
Alessandro Dragone <alessandro.dragone@beije.it>
2022-10-25 16:35:09 +0200

    Added navigation buttons in NavBar

--- src/components/hook/NavBar/NavBar.tsx
+++ src/components/hook/NavBar/NavBar.tsx
@@ -14,125 +14,156 @@ import PAGES from "../../../router/pages";
 import css from "./navBar.module.scss";
 
 interface State {
-  editor: boolean;
-  articles: boolean;
-  users: boolean;
+    editor: boolean;
+    articles: boolean;
+    users: boolean;
 }
 
 const initialState: State = {
-  editor: false,
-  articles: false,
-  users: false,
+    editor: false,
+    articles: false,
+    users: false,
 };
 
 //NAVBAR
 export default function NavBar() {
-  const [state, setState] = useState<State>(initialState);
+    const [state, setState] = useState<State>(initialState);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  //Funzioni per aprire e chiudere i collapse
-  const openEditor = (): void => {
-    setState({ editor: !state.editor, articles: false, users: false });
-  };
-
-  const openArticles = (): void => {
-    setState({ articles: !state.articles, editor: false, users: false });
-  };
-
-  const openUsers = (): void => {
-    setState({ users: !state.users, articles: false, editor: false });
-  };
-
-  //Navigazione
-  const handleNavigate =
-    () =>
-    (params: any): void => {
-      console.log(params);
+    //Funzioni per aprire e chiudere i collapse
+    const openEditor = (): void => {
+        setState({ editor: !state.editor, articles: false, users: false });
     };
 
-  return (
-    <>
-      <List className={css.container} component="nav">
-        <ListItemButton className={css.father} onClick={openEditor}>
-          <ListItemText primary="EDITOR SITO" />
-          {state.editor ? <ExpandMore /> : <ExpandLess />}
-        </ListItemButton>
+    const openArticles = (): void => {
+        setState({ articles: !state.articles, editor: false, users: false });
+    };
 
-        <Collapse in={state.editor} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton
-              className={css.child}
-              onClick={() => navigate(PAGES.editGeneral)}
-            >
-              <ListItemText primary="Generale" />
-            </ListItemButton>
+    const openUsers = (): void => {
+        setState({ users: !state.users, articles: false, editor: false });
+    };
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Home" />
-            </ListItemButton>
+    //Navigazione
+    const handleNavigate = (params: string) => (): void => {
+        navigate(params)
+    };
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="About" />
-            </ListItemButton>
+    return (
+        <>
+            <List className={css.container} component="nav">
+                <ListItemButton className={css.father} onClick={openEditor}>
+                    <ListItemText primary="EDITOR SITO" />
+                    {state.editor ? <ExpandMore /> : <ExpandLess />}
+                </ListItemButton>
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Supportaci" />
-            </ListItemButton>
+                <Collapse in={state.editor} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editGeneral)}
+                        >
+                            <ListItemText primary="Generale" />
+                        </ListItemButton>
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="FAQ" />
-            </ListItemButton>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editHome)}
+                        >
+                            <ListItemText primary="Home" />
+                        </ListItemButton>
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Social" />
-            </ListItemButton>
-          </List>
-        </Collapse>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editAbout)}
+                        >
+                            <ListItemText primary="About" />
+                        </ListItemButton>
 
-        <ListItemButton className={css.father}>
-          <ListItemText primary="EVENTI" />
-        </ListItemButton>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editSupportUs)}
+                        >
+                            <ListItemText primary="Supportaci" />
+                        </ListItemButton>
 
-        <ListItemButton className={css.father} onClick={openArticles}>
-          <ListItemText primary="ARTICOLI" />
-          {state.articles ? <ExpandMore /> : <ExpandLess />}
-        </ListItemButton>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editFaq)}
+                        >
+                            <ListItemText primary="FAQ" />
+                        </ListItemButton>
 
-        <Collapse in={state.articles} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Gestisci articoli" />
-            </ListItemButton>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.editSocial)}
+                        >
+                            <ListItemText primary="Social" />
+                        </ListItemButton>
+                    </List>
+                </Collapse>
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Gestisci categorie" />
-            </ListItemButton>
-          </List>
-        </Collapse>
+                <ListItemButton
+                    className={css.father}
+                    onClick={handleNavigate(PAGES.events)}
+                >
+                    <ListItemText primary="EVENTI" />
+                </ListItemButton>
 
-        <ListItemButton className={css.father} onClick={openUsers}>
-          <ListItemText primary="UTENTI" />
-          {state.users ? <ExpandMore /> : <ExpandLess />}
-        </ListItemButton>
+                <ListItemButton className={css.father} onClick={openArticles}>
+                    <ListItemText primary="ARTICOLI" />
+                    {state.articles ? <ExpandMore /> : <ExpandLess />}
+                </ListItemButton>
 
-        <Collapse in={state.users} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Collaboratori" />
-            </ListItemButton>
+                <Collapse in={state.articles} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.articlesBlog)}
+                        >
+                            <ListItemText primary="Gestisci articoli" />
+                        </ListItemButton>
 
-            <ListItemButton className={css.child}>
-              <ListItemText primary="Volontari" />
-            </ListItemButton>
-          </List>
-        </Collapse>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.articlesCategories)}
+                        >
+                            <ListItemText primary="Gestisci categorie" />
+                        </ListItemButton>
+                    </List>
+                </Collapse>
 
-        <ListItemButton className={css.father}>
-          <ListItemText primary="DONAZIONI" />
-        </ListItemButton>
-      </List>
-      <Outlet />
-    </>
-  );
+                <ListItemButton className={css.father} onClick={openUsers}>
+                    <ListItemText primary="UTENTI" />
+                    {state.users ? <ExpandMore /> : <ExpandLess />}
+                </ListItemButton>
+
+                <Collapse in={state.users} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.usersCollaborators)}
+                        >
+                            <ListItemText primary="Collaboratori" />
+                        </ListItemButton>
+
+                        <ListItemButton
+                            className={css.child}
+                            onClick={handleNavigate(PAGES.usersVolunteers)}
+                        >
+                            <ListItemText primary="Volontari" />
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+
+                <ListItemButton
+                    className={css.father}
+                    onClick={handleNavigate(PAGES.donations)}
+                >
+                    <ListItemText primary="DONAZIONI" />
+                </ListItemButton>
+            </List>
+            <Outlet />
+        </>
+    );
 }
