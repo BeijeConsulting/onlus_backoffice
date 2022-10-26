0472c0a31ebf00ed070db6b3e44d4b40a0376db4
MilanBeije <milan.stojkovic@beije.it>
2022-10-26 12:59:06 +0200

    fixed outlet

--- src/Test.tsx
+++ src/Test.tsx
@@ -3,7 +3,7 @@ import style from "./assets/styles/common.module.scss";
 
 //mui
 import { StyledEngineProvider } from "@mui/material";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 //components
 import LabelText from "./components/functional/labelText/LabelText";
@@ -12,58 +12,25 @@ import CustomTextField from "./components/functional/textField/CustomTextField";
 import ButtonAddFile from "./components/functional/buttonAddFile/ButtonAddFile";
 
 function Test() {
-
-  const onSelectedFile = (value:string) :void =>{
-    
-  }
+  const onSelectedFile = (value: string): void => {};
 
   return (
-    <StyledEngineProvider injectFirst>
-      <Box className="style.App">
-        <Box className={style.container}>
-          <header></header>
+    <Box className={style.component}>
+      <Box className={style.doubleComponent}>
+        <Box className={style.left}>
+          <LabelText>
+            <Title text={"Nome sito"} textInfo={"Inserisci il nome del sito"} />
+            <CustomTextField error={false}></CustomTextField>
+            <CustomTextField error={false}></CustomTextField>
 
-          <Box className={style.content}>
-            <nav></nav>
-
-            <main>
-              <Box className={style.component}>
-                <Box className={style.doubleComponent}>
-                  <Box className={style.left}>
-                    
-                    <LabelText>
-                      <Title
-                        text={"Nome sito"}
-                        textInfo={"Inserisci il nome del sito"}
-                      />
-                      <CustomTextField error={false}></CustomTextField>
-                      <CustomTextField error={false}></CustomTextField>
-
-                      <ButtonAddFile
-                        callback={onSelectedFile}></ButtonAddFile>
-
-                    </LabelText>
-
-
-
-                  </Box>
-                  <Box className={style.right}>
-                    
-                  </Box>
-                </Box>
-
-                <Box className={style.singleComponent}>
-                  
-                </Box>
-
-              </Box>
-            </main>
-          </Box>
-
-          <footer></footer>
+            <ButtonAddFile callback={onSelectedFile}></ButtonAddFile>
+          </LabelText>
         </Box>
+        <Box className={style.right}></Box>
       </Box>
-    </StyledEngineProvider>
+
+      <Box className={style.singleComponent}></Box>
+    </Box>
   );
 }
 
