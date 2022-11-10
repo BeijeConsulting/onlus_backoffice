d14e1305830078cf05d174263a25bfd0f4804ea2
MilanBeije <milan.stojkovic@beije.it>
2022-11-10 12:15:41 +0100

    updated config and API routes

--- src/services/api/apiRoutes.js
+++ src/services/api/apiRoutes.js
@@ -1,18 +1,16 @@
 const APIROUTES = {
-    GENERAL: "/general",
-    HOME: "/home",
-    PERSONALAREA: "/personal-area",
-    SUPPORT: "/support",
-    FAQ: "/faq",
-    ABOUT: "/about",
-    ARTICLES: "/articles",
-    CATEGORIES: "/categories",
-    DONATIONS: "/donations",
-    EVENTS: "/events",
-    SOCIALS: "/socials",
-    USERACCOUNTS: "/user-accounts",
-
-
-}
+  GENERAL: "/general",
+  HOME: "/home",
+  PERSONALAREA: "/personal-area",
+  SUPPORT: "/support",
+  FAQ: "/faq",
+  ABOUT: "/about",
+  ARTICLES: "/articles",
+  CATEGORIES: "/categories",
+  DONATIONS: "/donations",
+  EVENTS: "/events",
+  SOCIALS: "/socials",
+  USERACCOUNTS: "/user-accounts",
+};
 
 export default APIROUTES;
