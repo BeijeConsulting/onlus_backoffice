f17382df864ab4d5df0ba2942ae9268cd8d6b830
mattia laurenzana <mattia.laurenzana@beije.it>
2022-11-16 12:01:02 +0100

    Fix types file

--- src/utils/mockup/types.ts
+++ src/utils/mockup/types.ts
@@ -20,14 +20,61 @@ export type Article = {
   category: Array<Category>;
   content: Array<ArticleContent>;
   cover: string;
-  date: string;
+  date?: string;
   email?: string;
   id?: number;
   name?: string;
-  status: string;
+  status?: string;
   surname?: string;
   title: string;
   userId?: number;
+};
+
+export type Banner = {
+  btnText1: string;
+  btnText2: string;
+  id?: number;
+  link: string;
+  subtitle: string;
+  title: string;
+};
+
+export type Contacts = {
+  address: string;
+  email: string;
+  fiscalCode: string;
+  id?: number;
+  site: string;
+  vatNumber: string;
+};
+
+export type Palette = {
+  bgColor: string;
+  id?: number;
+  name: string;
+  textColor: string;
+};
+
+export type SectionWork = {
+  email: string;
+  text: string;
+};
+
+export type Customization = {
+  websiteName: string;
+  logo: string;
+  banner: Banner;
+  contacts: Contacts;
+  palette: Array<Palette>;
+  sectionWork: SectionWork;
+};
+
+export type Donation = {
+  name: string;
+  surname: string;
+  userId: number;
+  amount: number;
+  donationDate: string;
 };
 
 export type Event = {
@@ -39,3 +86,28 @@ export type Event = {
   title?: string;
   id?: Number | null;
 };
+
+export type AboutContent = {
+  id: number,
+  mediaContent: string,
+  mediaType: string,
+  paragraph: string
+}
+
+export type AboutHero = {
+  image: string,
+  subtitle: string,
+  text: string
+}
+
+export type AboutTitle = {
+  id: number,
+  title: string
+}
+
+export type AboutType = {
+  content: Array<AboutContent>,
+  hero: AboutHero,
+  title: AboutTitle
+}
+
