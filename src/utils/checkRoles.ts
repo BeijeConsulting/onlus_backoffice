d558773500205fc26417598bef32019c113b2f11
LAPTOP-LOTTO\botto <luca.bottoli@beije.it>
2022-11-18 11:48:07 +0100

    bug fix

--- src/utils/checkRoles.ts
+++ src/utils/checkRoles.ts
@@ -1,9 +1,9 @@
 const checkRole = (params: any): string => {
-    if (params.includes('SUPERADMIN')) {
+    if (params?.includes('SUPERADMIN')) {
         return '3'
-    } else if (params.includes('ADMIN')) {
+    } else if (params?.includes('ADMIN')) {
         return '2'
-    } else if (params.includes('BLOGGER')){
+    } else if (params?.includes('BLOGGER')){
         return '4'
     } else {
         return '1'
